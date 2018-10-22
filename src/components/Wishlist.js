6c60e2395c25b49e26196159785a4a9010bc560b
Dan DeMass <demassd@gmail.com>
2018-10-22 13:53:14 -0600

    3rd commit

---
+++ src/components/Wishlist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WishList from './WishListForm'
+import Items from './Items'
+
+const WishList = () => (
+    <div>
+        <WishListForm />
+        <Items />
+    </div>
+)
+
+export default () => <div></div>
